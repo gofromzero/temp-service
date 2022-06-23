c44b115d979548f4817d4369e5fbfc04c38d02f0
spume <gofromzero@gmail.com>
2022-06-23 13:49:07 +0800

    feat(project): add some sample

---
+++ apps/wechat/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "temp-service/apps/wechat/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
--- script/gencode.sh
+++ script/gencode.sh
@@ -1,5 +1,9 @@
 
 
-goctl rpc new {name} --home ..\..\data\goctl\temp
+goctl rpc new {name} --home ..\..\deploy\goctl\temp
 
-goctl api go -api {name}.api -dir . -style goZero --home ..\..\data\goctl\temp
+goctl api go -api {name}.api -dir . -style goZero --home ..\..\deploy\goctl\temp
+
+# model
+
+goctl model mysql datasource -url="root:123456@tcp(127.0.0.1:33306)/test" -table="*"  -dir="./model" -c
